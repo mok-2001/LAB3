49dd92fb73f3a8d5b8aef587a8bb859e22ca82cc
mok-2001 <thanathat.ana@mail.kmutt.ac.th>
2022-04-21 15:49:54 +0700

    finish Kalmanfilter and experiment

--- LAB3/Core/Src/main.c
+++ LAB3/Core/Src/main.c
@@ -22,8 +22,9 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #define _USE_MATH_DEFINES
-#include "math.h"
-#include "kalman.h"
+#define ARM_MATH_CM4
+#include "arm_math.h"					//include library for matrix calculation
+#include "math.h"						//include library for Pi value
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -48,18 +49,168 @@ TIM_HandleTypeDef htim3;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-uint64_t _micros = 0;
-float DegRel =0;
-float DegAbs =0;
-float RadRel =0;
-float RadAbs =0;
-float unwrap=0;
-float unwraprad = 0;
+uint64_t _micros = 0;							//time in microsecond
+float DegRel =0;								//Degree Relative
+float DegAbs =0;								//Degree Absolute
+float RadRel =0;								//Radian Relative
+float RadAbs =0;								//Radian Absolute
+float unwrap=0;									//Degree Unwrap
+float unwraprad = 0;							//Radian Unwrap
+//Variable for Unwrapping function
 float Po ;
 float Polast;
 float Pnlast;
+//Variable for velocity approximate function
 float omega=0;
 float tetalast=0;
+//Variable for kalman filter
+float Rad_predict = 0;
+float Omega_predict = 0;
+//sample time in kalman
+const float delta_T = 0.01;
+
+//Kalman All Matrix define
+
+float32_t X_f32[3] =//initial condition
+{
+  /* Const,   numTaps,   blockSize,   numTaps*blockSize */
+  0.0,
+  0.0,
+  0.0,
+};
+float32_t U_f32[1] =//initial condition
+{
+  /* Const,   numTaps,   blockSize,   numTaps*blockSize */
+  0.0
+};
+float32_t A_f32[9] =
+{
+  /* Const,   numTaps,   blockSize,   numTaps*blockSize */
+  1.0,     delta_T,   delta_T*delta_T/2.0,
+  0.0,     1.0,      delta_T,
+  0.0,     0.0,      1.0
+
+};
+float32_t B_f32[3] =
+{
+  /* Const,   numTaps,   blockSize,   numTaps*blockSize */
+  0,
+  0,
+  0
+};
+float32_t G_f32[3] =
+{
+  /* Const,   numTaps,   blockSize,   numTaps*blockSize */
+  delta_T*delta_T*delta_T/6.0,
+  delta_T*delta_T/2.0,
+  delta_T
+};
+float32_t Q_f32[1] =
+{
+  /* Const,   numTaps,   blockSize,   numTaps*blockSize */
+  0.00828
+};
+//mesurement
+float32_t C_f32[3] =
+{
+  /* Const,   numTaps,   blockSize,   numTaps*blockSize */
+  1,	0,		0
+};
+/*const float32_t D_f32[1] =
+{
+  // Const,   numTaps,   blockSize,   numTaps*blockSize
+  0
+};*/
+float32_t R_f32[1] =
+{
+  /* Const,   numTaps,   blockSize,   numTaps*blockSize */
+  0.01
+};
+float32_t Z_f32[1] =//input unwrap
+{
+  //Const,   numTaps,   blockSize,   numTaps*blockSize
+  0.0
+};
+float32_t Y_f32[1] =
+{
+  //Const,   numTaps,   blockSize,   numTaps*blockSize
+  0.0
+};
+float32_t AT_f32[9];//3X3
+float32_t CT_f32[3];//3X1
+float32_t GT_f32[3];//1X3
+float32_t Xp_f32[3];//3X1
+float32_t Pp_f32[9];//3X3
+float32_t P_f32[9]=
+	{
+		  /* Const,   numTaps,   blockSize,   numTaps*blockSize */
+		  0.0,	0.0,	0.0,
+		  0.0,	0.0,	0.0,
+		  0.0,	0.0,	0.0,
+	};//3X3
+float32_t I_f32[9]=
+	{
+		  /* Const,   numTaps,   blockSize,   numTaps*blockSize */
+		  1.0,	0.0,	0.0,
+		  0.0,	1.0,	0.0,
+		  0.0,	0.0,	1.0,
+	};//3X3
+float32_t S_f32[1];//1X1
+float32_t K_f32[3];//3X1
+float32_t GQGt_f32[9];//3X3
+float32_t GQ_f32[3];//3X1
+//calculation
+float32_t AX_f32[3];//3X1
+float32_t BU_f32[3];//3X1
+//Xp
+float32_t AP_f32[9];//3X3
+float32_t APAt_f32[9];//3X3
+//Pp
+float32_t AX_f32[3];//3X1
+float32_t CXp_f32[1];//1X1
+float32_t CPp_f32[3];//1X3
+float32_t CPpCt_f32[3];//1X3
+
+float32_t PpCt_f32[3];//3X1
+float32_t S_inv_f32[1];//1X1
+float32_t KY_f32[3];//3X1
+float32_t KC_f32[9];//3X3
+float32_t I_KC_f32[9];//3X3
+
+arm_matrix_instance_f32 A;      /* Matrix A Instance */
+arm_matrix_instance_f32 AT;     /* Matrix AT(A transpose) */
+arm_matrix_instance_f32 B;      /* Matrix B Instance */
+arm_matrix_instance_f32 G;      /* Matrix G Instance */
+arm_matrix_instance_f32 GT;     /* Matrix GT(G transpose) */
+arm_matrix_instance_f32 Q;      /* Matrix Q Instance */
+arm_matrix_instance_f32 C;      /* Matrix C Instance */
+arm_matrix_instance_f32 CT;     /* Matrix CT(C transpose) */
+arm_matrix_instance_f32 R;      /* Matrix R Instance */
+arm_matrix_instance_f32 Y;      /* Matrix Y Instance */
+arm_matrix_instance_f32 X;      /* Matrix X Instance */
+arm_matrix_instance_f32 Xp;     /* Matrix Xp(X predict) */
+arm_matrix_instance_f32 P;      /* Matrix P Instance */
+arm_matrix_instance_f32 Pp;     /* Matrix Pp(P predict) */
+arm_matrix_instance_f32 U;      /* Matrix U Instance */
+arm_matrix_instance_f32 K;      /* Matrix K(kalman gain) Instance */
+arm_matrix_instance_f32 S;      /* Matrix S Instance */
+arm_matrix_instance_f32 Z;      /* Matrix Z Instance */
+arm_matrix_instance_f32 GQ;     /* Matrix GQ Instance */
+arm_matrix_instance_f32 GQGt;   /* Matrix GQGt Instance */
+arm_matrix_instance_f32 AX;     /* Matrix Ax Instance */
+arm_matrix_instance_f32 BU;     /* Matrix BU Instance */
+arm_matrix_instance_f32 APAt;   /* Matrix APAt Instance */
+arm_matrix_instance_f32 AP;     /* Matrix AP Instance */
+arm_matrix_instance_f32 CXp;    /* Matrix CXp Instance */
+arm_matrix_instance_f32 CPp;    /* Matrix CPp Instance */
+arm_matrix_instance_f32 CPpCt;  /* Matrix CPpCt Instance */
+arm_matrix_instance_f32 PpCt;   /* Matrix PpCt Instance */
+arm_matrix_instance_f32 S_inv;  /* Matrix S_inv Instance */
+arm_matrix_instance_f32 KY;     /* Matrix KY Instance */
+arm_matrix_instance_f32 I;      /* Matrix I Instance */
+arm_matrix_instance_f32 KC;     /* Matrix I Instance */
+arm_matrix_instance_f32 I_KC;   /* Matrix I Instance */
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -70,9 +221,11 @@ static void MX_TIM1_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
+//function prototype
 uint64_t micros();
 void UpdatePosition();
 float unwapping(float Pn);
+void velocityapproximate(float );
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -113,6 +266,58 @@ int main(void)
   MX_TIM2_Init();
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
+
+  uint32_t srcRows, srcColumns;
+  //set rows and columns for matrix
+  srcRows = 3;
+  srcColumns = 3;
+  arm_mat_init_f32(&A, srcRows, srcColumns, A_f32);
+  arm_mat_init_f32(&AT, srcRows, srcColumns, AT_f32);
+  arm_mat_trans_f32(&A, &AT);
+  arm_mat_init_f32(&GQGt, srcRows, srcColumns, GQGt_f32);
+  arm_mat_init_f32(&P, srcRows, srcColumns, P_f32);
+  arm_mat_init_f32(&Pp, srcRows, srcColumns, Pp_f32);
+  arm_mat_init_f32(&AP, srcRows, srcColumns, AP_f32);
+  arm_mat_init_f32(&APAt, srcRows, srcColumns, APAt_f32);
+  arm_mat_init_f32(&I, srcRows, srcColumns, I_f32);
+  arm_mat_init_f32(&KC, srcRows, srcColumns, KC_f32);
+  arm_mat_init_f32(&I_KC, srcRows, srcColumns, I_KC_f32);
+  srcRows = 1;
+  srcColumns = 3;
+  arm_mat_init_f32(&C, srcRows, srcColumns, C_f32);
+  arm_mat_init_f32(&GT, srcRows, srcColumns, GT_f32);
+  arm_mat_init_f32(&CPp, srcRows, srcColumns, CPp_f32);
+  arm_mat_init_f32(&CPpCt, srcRows, srcColumns, CPpCt_f32);
+  srcRows = 3;
+  srcColumns = 1;
+  arm_mat_init_f32(&X, srcRows, srcColumns, X_f32);
+  arm_mat_init_f32(&Xp, srcRows, srcColumns, Xp_f32);
+  arm_mat_init_f32(&CT, srcRows, srcColumns, CT_f32);
+  arm_mat_trans_f32(&C, &CT);
+  arm_mat_init_f32(&G, srcRows, srcColumns, G_f32);
+  arm_mat_trans_f32(&G, &GT);
+  arm_mat_init_f32(&B, srcRows, srcColumns, B_f32);
+  arm_mat_init_f32(&K, srcRows, srcColumns, K_f32);
+  arm_mat_init_f32(&GQ, srcRows, srcColumns, GQ_f32);
+  arm_mat_init_f32(&AX, srcRows, srcColumns, AX_f32);
+  arm_mat_init_f32(&BU, srcRows, srcColumns, BU_f32);
+  arm_mat_init_f32(&CPpCt, srcRows, srcColumns, CPpCt_f32);
+  arm_mat_init_f32(&PpCt, srcRows, srcColumns, PpCt_f32);
+  arm_mat_init_f32(&KY, srcRows, srcColumns, KY_f32);
+
+  srcRows = 1;
+  srcColumns = 1;
+  arm_mat_init_f32(&Q, srcRows, srcColumns, Q_f32);
+  arm_mat_init_f32(&U, srcRows, srcColumns, U_f32);
+  arm_mat_init_f32(&R, srcRows, srcColumns, R_f32);
+  arm_mat_init_f32(&Y, srcRows, srcColumns, Y_f32);
+  arm_mat_init_f32(&S, srcRows, srcColumns, S_f32);
+  arm_mat_init_f32(&Z, srcRows, srcColumns, Z_f32);
+  arm_mat_init_f32(&S_inv, srcRows, srcColumns, S_inv_f32);
+  arm_mat_mult_f32(&G,&Q,&GQ);
+  arm_mat_mult_f32(&GQ,&GT,&GQGt);
+  arm_mat_init_f32(&CXp, srcRows, srcColumns, CXp_f32);
+
   	HAL_TIM_Base_Start_IT(&htim3);
 	HAL_TIM_Base_Start_IT(&htim2);
 	HAL_TIM_Encoder_Start(&htim1, TIM_CHANNEL_1);
@@ -125,7 +330,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  UpdatePosition();
+	  UpdatePosition();  //Update Degree and radian from QEI
   }
   /* USER CODE END 3 */
 }
@@ -383,14 +588,19 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 void UpdatePosition(){
 	static uint64_t TimestampCalculationLoop  =0;
-	if(micros() -TimestampCalculationLoop >=1000){
+	if(micros() -TimestampCalculationLoop >=1000){	//Update every 1000 Hz
 		uint16_t RawRead = TIM1->CNT;
 		DegRel = (RawRead/3072.0)*360;
 		DegAbs =((RawRead%3072)/3072.0)*360;
 
 		RadRel =(RawRead/3072.0)*(2*M_PI);
 		RadAbs =((RawRead%3072)/3072.0)*(2*M_PI);
+
 	}
+}
+void velocityapproximate(float teta){
+	omega = 100.0*(teta-tetalast);
+	tetalast = teta;
 }
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
@@ -401,14 +611,46 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	if (htim == &htim3){
 			unwrap = unwapping(DegRel);
 			unwraprad = unwrap*M_PI/180.0;
+			velocityapproximate(unwraprad);
+			// Kalman Calcultation
+			Z_f32[0] = unwraprad;
+			//Xp
+			arm_mat_mult_f32(&A,&X,&AX);
+			arm_mat_mult_f32(&B,&U,&BU);
+			arm_mat_add_f32(&AX,&BU,&Xp);
+			//Pp
+			arm_mat_mult_f32(&A,&P,&AP);
+			arm_mat_mult_f32(&AP,&AT,&APAt);
+			arm_mat_add_f32(&APAt,&GQGt,&Pp);
+			//Y
+			arm_mat_mult_f32(&C,&Xp,&CXp);
+			arm_mat_sub_f32(&Z,&CXp,&Y);
+			//S
+			arm_mat_mult_f32(&C,&Pp,&CPp);
+			arm_mat_mult_f32(&CPp,&CT,&CPpCt);
+			arm_mat_add_f32(&CPpCt,&R,&S);
+			//K
+			arm_mat_mult_f32(&Pp,&CT,&PpCt);
+			arm_mat_inverse_f32(&S,&S_inv);
+			arm_mat_mult_f32(&PpCt,&S_inv,&K);
+			//X
+			arm_mat_mult_f32(&K,&Y,&KY);
+			arm_mat_add_f32(&KY,&Xp,&X);
+			//P
+			arm_mat_mult_f32(&K,&C,&KC);
+			arm_mat_sub_f32(&I,&KC,&I_KC);
+			arm_mat_mult_f32(&I_KC,&Pp,&P);
+
+			Rad_predict = X_f32[0];
+			Omega_predict = X_f32[1];
 		}
 }
 uint64_t micros()
 {
 	return _micros + htim2.Instance->CNT;
 }
-float unwapping(float Pn){
-	static float Pmax = 7560;//360*21
+float unwapping(float Pn){		//Upwrapping
+	static float Pmax = 7560;   //360*21 (21 rounds 1 unwrap)
 	static float threshold = 7560*0.5;
 	if (Pn-Pnlast <= -threshold){
 		Po = Polast + Pmax;
